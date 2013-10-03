24de46136335619d1f478155f1a0fcb61eb4641c
Martin.Herndl@gmail.com <Martin.Herndl@gmail.com@efbda7e9-440c-2264-a170-92ca4005e7c7>
2013-10-03 08:40:54 +0000

    2 new options for vote settings: 1 send mail(s) only if voted before, 2 send reminder to vote if not done yet before voting ends

--- misc/send_notifier.php
+++ misc/send_notifier.php
@@ -6,38 +6,68 @@
 
 	$votes = getAllVotes();
 
-	// votes exist and valid
-	if ($votes && is_array($votes['venue']) && !empty($votes['venue'])) {
+	// get action paramter
+	if (count($argv) == 2)
+		$action = trim($argv[1]);
+	if (!$action)
+		$action = get_var('action');
+	if (!in_array($action, array('remind', 'notify')))
+		exit("error, parameter action [remind|notify] invalid!\n");
 
-		// build html and header info for mails
-		$html = vote_summary_html($votes, true);
-		$html .= '<div style="margin: 10px">' .
-			getTemperatureString(false, false) .
-		'</div>';
-		$html .= '<div style="margin: 10px">
-			<a href="' . SITE_URL . '">' . SITE_URL . '</a>
-		</div>';
+	// build mail headers
+	$headers = array();
+	$headers[] = "From: " . SITE_FROM_MAIL;
+	$headers[] = "MIME-Version: 1.0";
+	$headers[] = "Content-type: text/html; charset=utf-8";
+	$headers[] = "X-Mailer: PHP/" . phpversion();
+	$headers[] = "Precedence: bulk";
+	// mail footer
+	$footer = '<br /><div style="margin: 10px"><small>Die Benachrichtigungen können auf <a href="' . SITE_URL . '">' . SITE_URL . '</a> verändert/abgestellt werden.</small></div>';
 
-		$headers = array();
-		$headers[] = "From: " . SITE_FROM_MAIL;
-		$headers[] = "MIME-Version: 1.0";
-		$headers[] = "Content-type: text/html; charset=utf-8";
-		$headers[] = "Subject: Voting-Ergebnis";
-		$headers[] = "X-Mailer: PHP/" . phpversion();
-		$headers[] = "Precedence: bulk";
+	$voting_over_time_print = date('H:i', $voting_over_time);
 
-		// loop user emails and send
-		$emails = array_values(emails_get());
-		foreach ($emails as $email) {
+	// loop user configs and send emails
+	$email_configs = email_config_get();
+	foreach ((array)$email_configs as $ip => $email_config) {
+		$user = ip_anonymize($ip);
+		// get user config values
+		$email = isset($email_config['email']) ? $email_config['email'] : '';
+		$vote_reminder = isset($email_config['vote_reminder']) ? $email_config['vote_reminder'] : false;
+		$vote_reminder = filter_var($vote_reminder, FILTER_VALIDATE_BOOLEAN) ? 'checked="checked"' : '';
+		$voted_mail_only = isset($email_config['voted_mail_only']) ? $email_config['voted_mail_only'] : false;
+		$voted_mail_only = filter_var($voted_mail_only, FILTER_VALIDATE_BOOLEAN) ? 'checked="checked"' : '';
+
+		// user not voted, but wants mails only if votes => continue
+		if ($voted_mail_only && !isset($votes['venue'][$user]))
+			continue;
+
+		// notify, votes exist and valid
+		if ($action == 'notify' && $votes && !empty($votes['venue'])) {
+			// build html
+			$html = vote_summary_html($votes, true);
+			$html .= '<div style="margin: 10px">' .
+				getTemperatureString(false, false) .
+			'</div>';
+			$html .= $footer;
+
 			$success = mail($email, "Voting-Ergebnis", $html, implode("\r\n", $headers));
+			if ($success)
+				echo "sent email to $email";
+			else
+				echo "error sending email to $email";
+		}
+		// remind
+		else if ($action == 'remind' && $vote_reminder && !isset($votes['venue'][$user])) {
+			// build html
+			$html = "<div style='margin: 10px'>Das Voting endet um <b>$voting_over_time_print</b>. Bitte auf <a href='" . SITE_URL . "'><b>" . SITE_URL . "</b></a> voten!</div>";
+			$html .= $footer;
 
+			$success = mail($email, "Voting-Erinnerung", $html, implode("\r\n", $headers));
 			if ($success)
 				echo "sent email to $email";
 			else
 				echo "error sending email to $email";
 		}
 	}
-	else
-		echo "votes not valid or recent";
 
 ?>
