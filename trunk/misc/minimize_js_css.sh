d0aae94c2559f057e8780c2865bc3654de62cce3
Martin.Herndl@gmail.com <Martin.Herndl@gmail.com@efbda7e9-440c-2264-a170-92ca4005e7c7>
2014-03-07 17:47:22 +0000

    statistics html5 updates, remove tagcloud, use leaner head.load.js instead of full head.js

--- trunk/misc/minimize_js_css.sh
+++ trunk/misc/minimize_js_css.sh
@@ -6,6 +6,5 @@ java -jar $BASEDIR/yuicompressor-2.4.8.jar --type css $BASEDIR/../public/css/sty
 
 java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $BASEDIR/../public/js/scripts.js -o $BASEDIR/../public/js/scripts-min.js
 java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $BASEDIR/../public/js/jquery.cookie.js -o $BASEDIR/../public/js/jquery.cookie-min.js
-java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $BASEDIR/../public/js/jquery.tagcloud.js -o $BASEDIR/../public/js/jquery.tagcloud-min.js
 java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $BASEDIR/../public/js/jquery.dataTables.js -o $BASEDIR/../public/js/jquery.dataTables-min.js
 java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $BASEDIR/../public/js/jquery.ui.datepicker-de.js -o $BASEDIR/../public/js/jquery.ui.datepicker-de-min.js
