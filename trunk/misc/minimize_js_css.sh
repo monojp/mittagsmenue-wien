fe7ba8687b7d69ffdafef0dea4db9982305e6b6d
Martin.Herndl@gmail.com <Martin.Herndl@gmail.com@efbda7e9-440c-2264-a170-92ca4005e7c7>
2014-07-28 16:57:21 +0000

    missing minimizer script

--- trunk/misc/minimize_js_css.sh
+++ trunk/misc/minimize_js_css.sh
@@ -1,11 +1,23 @@
 #!/bin/sh
 
 BASEDIR=$(dirname $0)
+JSDIR=$BASEDIR/../public/js/
+JSDIR=$(readlink -f $JSDIR)
+CSSDIR=$BASEDIR/../public/css/
+CSSDIR=$(readlink -f $CSSDIR)
 
-java -jar $BASEDIR/yuicompressor-2.4.8.jar --type css $BASEDIR/../public/css/styles.css -o $BASEDIR/../public/css/styles-min.css
-java -jar $BASEDIR/yuicompressor-2.4.8.jar --type css $BASEDIR/../public/css/black-tie/jquery-ui-1.10.4.custom.css -o $BASEDIR/../public/css/black-tie/jquery-ui-1.10.4.custom.min.css
+# concat main css
+cat $CSSDIR/basic.css > $CSSDIR/styles.css
+cat $CSSDIR/black-tie/jquery-ui.min.css >> $CSSDIR/styles.css
+# build minimized stylesheet
+java -jar $BASEDIR/yuicompressor-2.4.8.jar --type css $CSSDIR/styles.css -o $CSSDIR/styles-min.css
 
-java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $BASEDIR/../public/js/scripts.js -o $BASEDIR/../public/js/scripts-min.js
-java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $BASEDIR/../public/js/jquery.cookie.js -o $BASEDIR/../public/js/jquery.cookie-min.js
-java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $BASEDIR/../public/js/jquery.dataTables.js -o $BASEDIR/../public/js/jquery.dataTables-min.js
-java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $BASEDIR/../public/js/jquery.ui.datepicker-de.js -o $BASEDIR/../public/js/jquery.ui.datepicker-de-min.js
+# concat main js
+cat $JSDIR/jquery-1.11.1.min.js > $JSDIR/scripts.js
+cat $JSDIR/jquery-ui.min.js >> $JSDIR/scripts.js
+cat $JSDIR/basic.js >> $JSDIR/scripts.js
+cat $JSDIR/jquery.cookie.js >> $JSDIR/scripts.js
+cat $JSDIR/jquery.dataTables.js >> $JSDIR/scripts.js
+cat $JSDIR/jquery.ui.datepicker-de.js >> $JSDIR/scripts.js
+# build minimized script
+java -jar $BASEDIR/yuicompressor-2.4.8.jar --type js $JSDIR/scripts.js -o $JSDIR/scripts-min.js
