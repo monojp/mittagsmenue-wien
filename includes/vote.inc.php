bc77ea2827f7514e1302b787dd5004fe7629456c
Martin.Herndl@gmail.com <Martin.Herndl@gmail.com@efbda7e9-440c-2264-a170-92ca4005e7c7>
2014-10-21 08:09:52 +0000

    reminder mail: small update, cleanup in venue website and title getter

--- includes/vote.inc.php
+++ includes/vote.inc.php
@@ -115,6 +115,40 @@ function getUserVote() {
 	return isset($votes['venue'][get_identifier_ip()]) ? $votes['venue'][get_identifier_ip()] : null;
 }
 
+// tries to get the website from the venue class
+// searches nearplace caches or the real venue entries
+// and uses a simple google search as fallback
+function getWebsiteFromVenueClass($venue_class) {
+	if (class_exists($venue_class)) {
+		$venueTmp = new $venue_class;
+		return $venueTmp->url;
+		$venue_title = '<a href="' . $venueTmp->url . '" target="_blank" title="Homepage" style="color: inherit ! important">' . htmlspecialchars($venueTmp->title) . '</a>';
+	}
+	else {
+		// query nearplace cache (finds fake venues)
+		$website = nearplace_cache_search($venue_class);
+		$website = isset($website['website']) ? $website['website'] : null;
+		// query nearplace details cache (finds already clicked on real venues)
+		if ($website === null) {
+			$website = nearplace_details_cache_search($venue_class);
+			$website = isset($website['website']) ? $website['website'] : null;
+		}
+		// google fallback
+		if ($website === null)
+			$website = 'https://www.google.com/search?q=' . urlencode($venue_class);
+		return $website;
+	}
+}
+
+function getTitleFromVenueClass($venue_class) {
+	if (class_exists($venue_class)) {
+		$venueTmp = new $venue_class;
+		return $venueTmp->title;
+	}
+	else
+		return $venue_class;
+}
+
 function vote_summary_html($votes, $display_menus = false, $show_js_actions = true) {
 	$html = '';
 
@@ -186,24 +220,9 @@ function vote_summary_html($votes, $display_menus = false, $show_js_actions = tr
 
 			// replace classnames with real venue titles
 			foreach ($upVotes as &$venue_class) {
-				if (class_exists($venue_class)) {
-					$venueTmp = new $venue_class;
-					$venue_title = '<a href="' . $venueTmp->url . '" target="_blank" title="Homepage" style="color: inherit ! important">' . htmlspecialchars($venueTmp->title) . '</a>';
-				}
-				else {
-					// query nearplace cache (finds fake venues)
-					$website = nearplace_cache_search($venue_class);
-					$website = isset($website['website']) ? $website['website'] : null;
-					// query nearplace details cache (finds already clicked on real venues)
-					if ($website === null) {
-						$website = nearplace_details_cache_search($venue_class);
-						$website = isset($website['website']) ? $website['website'] : null;
-					}
-					// google fallback
-					if ($website === null)
-						$website = 'https://www.google.com/search?q=' . urlencode($venue_class);
-					$venue_title = '<a href="' . $website . '" target="_blank" title="Homepage" style="color: inherit ! important">' . htmlspecialchars($venue_class) . '</a>';
-				}
+				$website = getWebsiteFromVenueClass($venue_class);
+				$title = htmlspecialchars(getTitleFromVenueClass($venue_class));
+				$venue_title = "<a href='${website}' target='_blank' title='Homepage' style='color: inherit ! important'>${title}</a>";
 				// current user => add delete functionality
 				if ($show_js_actions && $user == get_identifier_ip())
 					$venue_title .= " <sup title='Löschen'><a href='javascript:void(0)' onclick='vote_delete_part(\"{$venue_class}\")' title='' style='color: red ! important'>x</a></sup>";
@@ -216,24 +235,9 @@ function vote_summary_html($votes, $display_menus = false, $show_js_actions = tr
 
 			// replace classnames with real venue titles
 			foreach ($downVotes as &$venue_class) {
-				if (class_exists($venue_class)) {
-					$venueTmp = new $venue_class;
-					$venue_title = '<a href="' . $venueTmp->url . '" target="_blank" title="Homepage" style="color: inherit ! important">' . htmlspecialchars($venueTmp->title) . '</a>';
-				}
-				else {
-					// query nearplace cache (finds fake venues)
-					$website = nearplace_cache_search($venue_class);
-					$website = isset($website['website']) ? $website['website'] : null;
-					// query nearplace details cache (finds already clicked on real venues)
-					if ($website === null) {
-						$website = nearplace_details_cache_search($venue_class);
-						$website = isset($website['website']) ? $website['website'] : null;
-					}
-					// google fallback
-					if ($website === null)
-						$website = 'https://www.google.com/search?q=' . urlencode($venue_class);
-					$venue_title = '<a href="' . $website . '" target="_blank" title="Homepage" style="color: inherit ! important">' . htmlspecialchars($venue_class) . '</a>';
-				}
+				$website = getWebsiteFromVenueClass($venue_class);
+				$title = htmlspecialchars(getTitleFromVenueClass($venue_class));
+				$venue_title = "<a href='${website}' target='_blank' title='Homepage' style='color: inherit ! important'>${title}</a>";
 				// current user => add delete functionality
 				if ($show_js_actions && $user == get_identifier_ip())
 					$venue_title .= " <sup title='Löschen'><a href='javascript:void(0)' onclick='vote_delete_part(\"{$venue_class}\")' style='color: red ! important'>x</a></sup>";
@@ -291,13 +295,9 @@ function vote_summary_html($votes, $display_menus = false, $show_js_actions = tr
 		foreach ($venue_rating_final as $rating => $venues) {
 			// resolve class names to venue titles
 			foreach ($venues as &$venue_class) {
-				if (class_exists($venue_class)) {
-					$venueTmp = new $venue_class;
-					$venue_title = htmlspecialchars($venueTmp->title);
-				}
-				else
-					$venue_title = htmlspecialchars($venue_class);
-				$venue_class = $venue_title;
+				$website = getWebsiteFromVenueClass($venue_class);
+				$title = htmlspecialchars(getTitleFromVenueClass($venue_class));
+				$venue_class = "<a href='${website}' target='_blank' title='Homepage' style='color: inherit ! important'>${title}</a>";
 			}
 			unset($venue_class);
 
