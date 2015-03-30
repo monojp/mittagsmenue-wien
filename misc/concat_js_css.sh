103147cc1c121d65971bf748467b3c11691d7d6c
monojp <martin.herndl@gmail.com>
2015-03-30 15:50:14 +0200

    jQuery 2.1.3 update, jQuery UI 1.11.4 update

--- misc/concat_js_css.sh
+++ misc/concat_js_css.sh
@@ -12,7 +12,7 @@ cat $CSSDIR/jquery-ui.min.css >> $CSSDIR/styles.css
 cat $CSSDIR/jquery.dataTables.min.css >> $CSSDIR/styles.css
 
 # concat js
-cat $JSDIR/jquery-2.1.1.min.js > $JSDIR/scripts.js
+cat $JSDIR/jquery-2.1.3.min.js > $JSDIR/scripts.js
 cat $JSDIR/jquery-ui.min.js >> $JSDIR/scripts.js
 cat $JSDIR/basic.js >> $JSDIR/scripts.js
 cat $JSDIR/jquery.cookie.js >> $JSDIR/scripts.js
