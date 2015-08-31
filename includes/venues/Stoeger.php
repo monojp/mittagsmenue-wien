f4640fe5d52395753957577ee8c8eac577d6b299
Martin Herndl <martin.herndl@gmail.com>
2015-08-31 11:00:09 +0200

    Stoeger: added missing today_variants, cleanups

--- includes/venues/Stoeger.php
+++ includes/venues/Stoeger.php
@@ -4,16 +4,16 @@ class Stoeger extends FoodGetterVenue {
 
 	function __construct() {
 		$this->title             = 'Restaurant Stöger';
-		$this->title_notifier    = 'NEU';
-		$this->address           = 'Ramperstorffergasse 63, 1050 Wien';
-		$this->addressLat        = 48.190359;
-		$this->addressLng        = 16.354808;
-		$this->url               = 'http://www.zumstoeger.at/';
-		$this->dataSource        = 'http://www.zumstoeger.at/heute.php';
-		$this->menu              = 'http://www.zumstoeger.at/speisekarte.pdf';
+		$this->title_notifier = 'NEU';
+		$this->address = 'Ramperstorffergasse 63, 1050 Wien';
+		$this->addressLat = 48.190359;
+		$this->addressLng = 16.354808;
+		$this->url = 'http://www.zumstoeger.at/';
+		$this->dataSource = 'http://www.zumstoeger.at/heute.php';
+		$this->menu = 'http://www.zumstoeger.at/speisekarte.pdf';
 		$this->statisticsKeyword = 'stoeger';
-		$this->no_menu_days      = [ 0, 1, 6 ];
-		$this->lookaheadSafe     = true;
+		$this->no_menu_days = [ 0, 1, 6 ];
+		$this->lookaheadSafe = true;
 		//$this->price_nested_info = 'Mittagsteller mit Suppe / Tagessuppe / Mittagsteller';
 
 		parent::__construct();
@@ -41,23 +41,33 @@ class Stoeger extends FoodGetterVenue {
 					. getGermanMonthName() . date(' Y', $this->timestamp),
 			getGermanDayName() . date(',d.', $this->timestamp)
 					. getGermanMonthName() . date(' Y', $this->timestamp),
+			getGermanDayName() . date(', j. ', $this->timestamp)
+					. getGermanMonthName() . date(' Y', $this->timestamp),
+			getGermanDayName() . date(',j. ', $this->timestamp)
+					. getGermanMonthName() . date(' Y', $this->timestamp),
+			getGermanDayName() . date(', j.', $this->timestamp)
+					. getGermanMonthName() . date(' Y', $this->timestamp),
+			getGermanDayName() . date(',j.', $this->timestamp)
+					. getGermanMonthName() . date(' Y', $this->timestamp),
 		];
 	}
 
 	protected function parseDataSource() {
 		$dataTmp = html_get_clean($this->dataSource);
-		if (!$dataTmp)
+		if (!$dataTmp) {
 			return;
+		}
 		//return error_log($dataTmp);
 
 		// get menu data for the chosen day
 		$data = $this->parse_foods_inbetween_days($dataTmp, 'Spezialitäten');
-		if (!$data || is_numeric($data))
+		if (!$data || is_numeric($data)) {
 			return $this->data = $data;
+		}
 		//return error_log($data);
 
 		// set price
-		$this->price = [ $this->parse_prices_regex($dataTmp, [ '/\d{1},\d{2}/' ]) ];
+		$this->price = $this->parse_prices_regex($dataTmp, [ '/\d{1},\d{2}/' ]);
 		//return error_log(print_r($this->price, true));
 
 		return ($this->data = $data);
