monojp/mittagsmenue-wien

commit 45b908422345b018d511bbf36dcab2e4f5d63c04

common parser: converted "string_next_day" parameter to an array, improved cleaining of number makers which fixes strange replacement bugs (e.g. "1/Brathuhn" instead of "1/2 Brathuhn")

includes/FoodGetterVenue.php
@@ -457,6 +457,10 @@ abstract class FoodGetterVenue {
 		$today_variants = $this->get_today_variants();
 		//return error_log(print_r($today_variants, true));
 
+		// convert next_day to an array
+		if (!is_array($string_next_day)) {
+			$string_next_day = [ $string_next_day ];
+		}
 		// convert last_day_next to an array
 		if (!is_array($string_last_day_next)) {
 			$string_last_day_next = [ $string_last_day_next ];
@@ -478,7 +482,15 @@ abstract class FoodGetterVenue {
 		if ($posStart === false) {
 			return;
 		}
-		$posEnd = mb_stripos($data, $string_next_day, $posStart);
+		foreach ($string_next_day as $tomorrow) {
+			$posEnd = strposAfter($data, $tomorrow, $posStart);
+			// set date and stop if match found
+			if ($posEnd !== false) {
+				//error_log("'${tomorrow}' found on pos ${posEnd}");
+				$this->date = $tomorrow;
+				break;
+			}
+		}
 		//error_log("'${string_next_day}' search returned ${posEnd}");
 		// last day of the week (string)
 		if ($posEnd === false && !is_array($string_last_day_next)) {
@@ -539,7 +551,7 @@ abstract class FoodGetterVenue {
 			'1,', '2,', '3,', '4,',
 			'1.', '2.', '3.', '4.',
 			'1)', '2)', '3)', '4)',
-			'1 ', '2 ', '3 ', '4 ', // macht evtl probleme mit dingen wie "1/2 Brathuhn"
+			'1 ', '2 ', '3 ', '4 ',
 		];
 
 		$foods_title = [
@@ -674,15 +686,18 @@ abstract class FoodGetterVenue {
 		}
 		//return error_log($data) && false;
 
-		// replace common noise strings
-		$data = str_replace($number_markers, '', $data);
-
 		$foods = explode_by_array([ "\n", "\r" ], $data);
 		//return error_log(print_r($foods, true)) && false;
 
-		// strip price infos from foods
 		foreach ($foods as &$food) {
+			// strip price infos from foods
 			$food = preg_replace($regex_price, ' ', $food);
+			// replace common numbering strings (if they start the line)
+			foreach ($number_markers as $number_marker) {
+				if (startswith($food, $number_marker)) {
+					$food = mb_substr($food, count($number_marker) + 1);
+				}
+			}
 			$food = cleanText($food);
 		}
 		unset($food);

commit e468773c261df509b7812b1602267acd59e7fa6d

common parser: fixed an offset bug caused by 45b908422345b018d511bbf36dcab2e4f5d63c04
"strposAfter"-helper was used instead of the standard "mb_stripos"
which lead to getting the substring we're interested in offseted
by exactly the length of the "string_next_day" array element

includes/FoodGetterVenue.php
@@ -484,11 +484,10 @@ abstract class FoodGetterVenue {
 			return;
 		}
 		foreach ($string_next_day as $tomorrow) {
-			$posEnd = strposAfter($data, $tomorrow, $posStart);
-			// set date and stop if match found
+			$posEnd = mb_stripos($data, $tomorrow, $posStart);
+			// stop if match found
 			if ($posEnd !== false) {
 				//error_log("'${tomorrow}' found on pos ${posEnd}");
-				$this->date = $tomorrow;
 				break;
 			}
 		}
@@ -502,7 +501,7 @@ abstract class FoodGetterVenue {
 			foreach ($string_last_day_next as $last_day_next) {
 				$posEnd = mb_stripos($data, $last_day_next, $posStart);
 				if ($posEnd !== false) {
-					//error_log("'${last_day_next}' found on pos ${posEnd}");
+				//error_log("'${last_day_next}' found on pos ${posEnd}");
 					break;
 				}
 			}
@@ -511,7 +510,7 @@ abstract class FoodGetterVenue {
 			//error_log('no end found');
 			return;
 		 }
-		//return error_log($posEnd);
+		//return error_log($posEnd) && false;
 
 		$data = mb_substr($data, $posStart, $posEnd - $posStart);
 		//return error_log($data) && false;

includes/includes.php
@@ -94,8 +94,9 @@ function cacheSafeUrl($file) {
 }
 function strposAfter($haystack, $needle, $offset=0) {
 	$pos = mb_strpos($haystack, $needle, $offset);
-	if ($pos !== FALSE)
+	if ($pos !== false) {
 		$pos += mb_strlen($needle);
+	}
 	return $pos;
 }
 /*