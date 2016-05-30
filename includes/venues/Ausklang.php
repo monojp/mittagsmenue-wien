996ce003403babd1dd98fb5eb99e7dea68fc503e
Martin Herndl <martin@herndl.org>
2016-05-30 18:37:38 +0200

    Ausklang: code cleanups and parser improvements

--- includes/venues/Ausklang.php
+++ includes/venues/Ausklang.php
@@ -3,16 +3,16 @@
 class Ausklang extends FoodGetterVenue {
 
 	function __construct() {
-		$this->title             = 'aus:klang';
-		$this->address           = 'Kleistgasse 1, 1030 Wien';
-		$this->addressLat        = 48.1931822;
-		$this->addressLng        = 16.3906608;
-		$this->url               = 'http://ausklang.at/';
-		$this->dataSource        = 'http://ausklang.at/essen/menue.html';
-		$this->menu              = 'http://ausklang.at/essen.html';
+		$this->title = 'aus:klang';
+		$this->address = 'Kleistgasse 1, 1030 Wien';
+		$this->addressLat = 48.1931822;
+		$this->addressLng = 16.3906608;
+		$this->url = 'http://ausklang.at/';
+		$this->dataSource = 'http://ausklang.at/essen/menue.html';
+		$this->menu = 'http://ausklang.at/essen.html';
 		$this->statisticsKeyword = 'ausklang';
-		$this->no_menu_days      = [ 0, 6 ];
-		$this->lookaheadSafe     = true;
+		$this->no_menu_days = [ 0, 6 ];
+		$this->lookaheadSafe = true;
 		$this->price_nested_info = 'Vorspeise oder Dessert / Vorspeise und Dessert';
 
 		parent::__construct();
@@ -20,19 +20,23 @@ class Ausklang extends FoodGetterVenue {
 
 	protected function get_today_variants() {
 		return [
-			mb_strtolower(getGermanDayName()) . ', ' . date('d.', $this->timestamp) . ' ' . mb_strtolower(getGermanMonthName()),
+			mb_strtolower(getGermanDayName()) . ', ' . date('d.', $this->timestamp) . ' '
+					. mb_strtolower(getGermanMonthName()),
 		];
 	}
 
 	protected function parseDataSource() {
 		$dataTmp = html_get_clean($this->dataSource);
-		if (!$dataTmp)
+		if (!$dataTmp) {
 			return;
+		}
 
 		// get menu data for the chosen day
-		$data = $this->parse_foods_inbetween_days($dataTmp, getGermanDayName(1), 'menü');
-		if (!$data || is_numeric($data))
+		$data = $this->parse_foods_inbetween_days($dataTmp, getGermanDayName(1),
+				[ 'menü', 'men&uuml;' ]);
+		if (!$data || is_numeric($data)) {
 			return $this->data = $data;
+		}
 		//return error_log($data);
 
 		// set price
