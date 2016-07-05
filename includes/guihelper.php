fac2792e95736992f2717e04459f6727506d1b41
Martin Herndl <martin@herndl.org>
2016-07-05 10:32:23 +0200

    jquery: update 3.0.0

--- includes/guihelper.php
+++ includes/guihelper.php
@@ -81,7 +81,8 @@ function get_header_html() {
 	// javascript
 	if (!isset($_GET['minimal']))
 		$response .= '
-			<script src="' . cacheSafeUrl('/js/jquery-2.2.2.min.js') . '"></script>
+			<script src="' . cacheSafeUrl('/js/jquery-3.0.0.min.js') . '"></script>
+			<script src="' . cacheSafeUrl('/js/jquery-migrate-3.0.0.js') . '"></script>
 			<script src="' . cacheSafeUrl('/jquery_mobile/jquery.mobile-1.4.5.min.js') . '"></script>
 			<script src="' . cacheSafeUrl($basic_js) . '"></script>
 			<script src="' . cacheSafeUrl('/js/jquery.cookie.js') . '" async="async" onload="init_cookie()"></script>
