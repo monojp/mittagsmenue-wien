4184d46dd944899d8925ea003bb8f446bf01b5f9
Martin Herndl <martin@herndl.org>
2017-01-23 11:55:44 +0100

    update menu links for schlossquadrat venues

--- includes/venues/SchlossquadratSilberwirt.php
+++ includes/venues/SchlossquadratSilberwirt.php
@@ -9,7 +9,7 @@ class SchlossquadratSilberwirt extends FoodGetterVenue {
 		$this->addressLng = 16.3593266;
 		$this->url = 'http://www.silberwirt.at/';
 		$this->dataSource = 'http://www.silberwirt.at/pdf.php?days=23';
-		$this->menu = 'http://www.silberwirt.at/fileadmin/files/silberwirt/pdf/KaSilber160501.pdf';
+		$this->menu = 'http://www.silberwirt.at/fileadmin/files/silberwirt/pdf/KaSilber161001.pdf';
 		$this->no_menu_days = [ 0 ];
 		$this->lookaheadSafe = true;
 
