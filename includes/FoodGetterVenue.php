monojp/mittagsmenue-wien

commit ce0772e6b7773ecc4c5ac61374dd07c18bfd5793

Fix PHP 7.2 Warning

includes/FoodGetterVenue.php
@@ -721,7 +721,7 @@ abstract class FoodGetterVenue {
 			// replace common numbering strings (if they start the line)
 			foreach ($number_markers as $number_marker) {
 				if (startswith($food, $number_marker)) {
-					$food = mb_substr($food, count($number_marker) + 1);
+					$food = mb_substr($food, mb_strlen($number_marker) + 1);
 				}
 			}
 			$food = cleanText($food);

commit 11ab4fa9352763dd1de9edbec3f46c0bd04c5dac

Fix bug introduced in ce0772e6b7773ecc4c5ac61374dd07c18bfd5793

includes/FoodGetterVenue.php
@@ -721,7 +721,7 @@ abstract class FoodGetterVenue {
 			// replace common numbering strings (if they start the line)
 			foreach ($number_markers as $number_marker) {
 				if (startswith($food, $number_marker)) {
-					$food = mb_substr($food, mb_strlen($number_marker) + 1);
+					$food = mb_substr($food, strlen($number_marker));
 				}
 			}
 			$food = cleanText($food);