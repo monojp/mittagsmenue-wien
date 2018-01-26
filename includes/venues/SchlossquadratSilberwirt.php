3d59c3cc22f55b963491b359d3987e6c9c1da9ea
monojp <martin@herndl.org>
2018-01-26 19:00:42 +0100

    SchlossquadratCommon class for identical parts

--- includes/venues/SchlossquadratSilberwirt.php
+++ includes/venues/SchlossquadratSilberwirt.php
@@ -1,6 +1,6 @@
 <?php
 
-class SchlossquadratSilberwirt extends FoodGetterVenue {
+class SchlossquadratSilberwirt extends SchlossquadratCommon {
 
 	function __construct() {
 		$this->title = 'Silberwirt';
@@ -14,54 +14,6 @@ class SchlossquadratSilberwirt extends FoodGetterVenue {
 		$this->lookaheadSafe = true;
 
 		parent::__construct();
-	}
-
-/*
- * structure
- * Montag 18.5.
- *
- * Knoblauchcremesuppe
- * Fleischknödel mit Kümmelsaft und Sauerkraut
- *
- * Dienstag 19.5.
- *
- * Paradeisercremesuppe
- * Gebackenes Truthahnschnitzel in Nuß¸panade mit Erdäpfelsalat
- */
-
-	protected function get_today_variants() {
-		return [
-			getGermanDayName() . ' ' . date('j.n.', $this->timestamp),
-			getGermanDayName() . ' ' . date('j.n',  $this->timestamp),
-			/*date('j.n.', $this->timestamp),
-			date('j.n',  $this->timestamp),*/
-		];
-	}
-
-	protected function parseDataSource() {
-		$dataTmp = pdftohtml($this->dataSource);
-		if (!$dataTmp) {
-			return;
-		}
-		//return error_log($dataTmp);
-
-		$dataTmp = html_clean($dataTmp);
-
-		// get menu data for the chosen day
-		$dataTmp = $this->parse_foods_inbetween_days($dataTmp, getGermanDayName(1),
-				[], "\n", false);
-		//return error_log($dataTmp);
-		if (!$dataTmp || is_numeric($dataTmp)) {
-			return ($this->data = $dataTmp);
-		}
-
-		// get and set price
-		$dataPriceTmp = html_get_clean($this->url);
-		$price = $this->parse_prices_regex($dataPriceTmp, [ '/Mittags(teller|menü) um \d{1},\d{2}/' ]);
-		//return error_log(print_r($price, true));
-		$this->price = is_array($price) ? reset($price) : $price;
-
-		return ($this->data = $dataTmp);
 	}
 
 }
